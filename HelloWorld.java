640d6f463a9a13959294db4e93bbb4ceb359eae6
vijayshyamasundar <vijay.shyamasundar@infostretch.com>
2016-12-24 20:05:52 -0500

    hello vijay2

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
-      System.out.println("Hello, Vijay1");
+      System.out.println("Hello, Vijay2");
    }
 }
